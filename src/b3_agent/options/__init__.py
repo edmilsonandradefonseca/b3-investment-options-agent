a1476fa6679e9d89f2945740c9a81e07ffb87d2a
EDMILSON DE ANDRADE FONSECA <edmilson.afonseca@gmail.com>
2026-09-14 20:49:05 -0300

    feat: export unified options analysis and policy

--- src/b3_agent/options/__init__.py
+++ src/b3_agent/options/__init__.py
@@ -1,9 +1,14 @@
+from .analysis import OptionsAnalysis, OptionsAnalysisEngine
 from .call import CallAnalysisEngine, CallOpportunity
+from .policy import OptionsPolicy
 from .put import PutAnalysisEngine, PutOpportunity
 
 __all__ = [
     "CallAnalysisEngine",
     "CallOpportunity",
+    "OptionsAnalysis",
+    "OptionsAnalysisEngine",
+    "OptionsPolicy",
     "PutAnalysisEngine",
     "PutOpportunity",
 ]
