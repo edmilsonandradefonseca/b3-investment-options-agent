23640f672e99621342ea34c276cbef617f741a04
EDMILSON DE ANDRADE FONSECA <edmilson.afonseca@gmail.com>
2026-09-14 20:10:18 -0300

    feat: export investment price policy

--- src/b3_agent/valuation/__init__.py
+++ src/b3_agent/valuation/__init__.py
@@ -1,5 +1,11 @@
 from .bank import BankValuationEngine
 from .dcf import DCFValuationEngine
 from .multiples import MultiplesValuationEngine
+from .policy import InvestmentPricePolicy
 
-__all__ = ["BankValuationEngine", "DCFValuationEngine", "MultiplesValuationEngine"]
+__all__ = [
+    "BankValuationEngine",
+    "DCFValuationEngine",
+    "InvestmentPricePolicy",
+    "MultiplesValuationEngine",
+]
