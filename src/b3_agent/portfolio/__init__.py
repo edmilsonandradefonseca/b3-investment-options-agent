4fe71d19d2c89b1b56f9634f27c44426969b6880
EDMILSON DE ANDRADE FONSECA <edmilson.afonseca@gmail.com>
2026-09-14 22:04:21 -0300

    feat: export portfolio intelligence context

--- src/b3_agent/portfolio/__init__.py
+++ src/b3_agent/portfolio/__init__.py
@@ -1,10 +1,12 @@
 from .capital_risk import CapitalRiskEngine, CapitalRiskSnapshot
+from .context import PortfolioIntelligence, PortfolioIntelligenceEngine
 from .intelligence import PositionExposure, PositionIntelligenceEngine
 from .pnl import PnlEngine, PositionPnl
 from .position_intelligence import PositionAssessment, PositionIntelligenceEngine as PerPositionIntelligenceEngine
 
 __all__ = [
     "CapitalRiskEngine", "CapitalRiskSnapshot",
+    "PortfolioIntelligence", "PortfolioIntelligenceEngine",
     "PositionExposure", "PositionIntelligenceEngine", "PnlEngine", "PositionPnl",
     "PositionAssessment", "PerPositionIntelligenceEngine",
 ]
