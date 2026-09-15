f24035a097efc59d63b5b468e7905227b7ee60cf
EDMILSON DE ANDRADE FONSECA <edmilson.afonseca@gmail.com>
2026-09-14 22:02:39 -0300

    feat: export capital risk engine

--- src/b3_agent/portfolio/__init__.py
+++ src/b3_agent/portfolio/__init__.py
@@ -1,8 +1,10 @@
+from .capital_risk import CapitalRiskEngine, CapitalRiskSnapshot
 from .intelligence import PositionExposure, PositionIntelligenceEngine
 from .pnl import PnlEngine, PositionPnl
 from .position_intelligence import PositionAssessment, PositionIntelligenceEngine as PerPositionIntelligenceEngine
 
 __all__ = [
+    "CapitalRiskEngine", "CapitalRiskSnapshot",
     "PositionExposure", "PositionIntelligenceEngine", "PnlEngine", "PositionPnl",
     "PositionAssessment", "PerPositionIntelligenceEngine",
 ]
