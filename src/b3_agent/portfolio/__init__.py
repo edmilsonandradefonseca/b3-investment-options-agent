df8b248468a27158dde235267090f850e95f07ed
EDMILSON DE ANDRADE FONSECA <edmilson.afonseca@gmail.com>
2026-09-14 21:44:00 -0300

    feat: export position intelligence assessment

--- src/b3_agent/portfolio/__init__.py
+++ src/b3_agent/portfolio/__init__.py
@@ -1,4 +1,8 @@
 from .intelligence import PositionExposure, PositionIntelligenceEngine
 from .pnl import PnlEngine, PositionPnl
+from .position_intelligence import PositionAssessment, PositionIntelligenceEngine as PerPositionIntelligenceEngine
 
-__all__ = ["PositionExposure", "PositionIntelligenceEngine", "PnlEngine", "PositionPnl"]
+__all__ = [
+    "PositionExposure", "PositionIntelligenceEngine", "PnlEngine", "PositionPnl",
+    "PositionAssessment", "PerPositionIntelligenceEngine",
+]
