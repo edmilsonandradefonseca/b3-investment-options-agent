51bd5a5459a5b77db6c80a342cd4eefc1ed86761
EDMILSON DE ANDRADE FONSECA <edmilson.afonseca@gmail.com>
2026-09-15 21:45:01 -0300

    refactor: expose V3.1 orchestration contracts

--- src/b3_agent/orchestration/__init__.py
+++ src/b3_agent/orchestration/__init__.py
@@ -1,3 +1,12 @@
+from .contracts import B3State, OrchestratorRequest, OrchestratorResponse
+from .orchestrator import b3_orchestrator, configure_workflow
 from .workflow import build_workflow
 
-__all__ = ["build_workflow"]
+__all__ = [
+    "B3State",
+    "OrchestratorRequest",
+    "OrchestratorResponse",
+    "b3_orchestrator",
+    "build_workflow",
+    "configure_workflow",
+]
