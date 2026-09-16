e81d51da4ff9e417242e40c60bd92ccf048c14a9
EDMILSON DE ANDRADE FONSECA <edmilson.afonseca@gmail.com>
2026-09-15 21:58:29 -0300

    feat: expose V3.1 runtime composition root

--- src/b3_agent/orchestration/__init__.py
+++ src/b3_agent/orchestration/__init__.py
@@ -1,5 +1,6 @@
 from .contracts import B3State, OrchestratorRequest, OrchestratorResponse
 from .orchestrator import b3_orchestrator, configure_workflow
+from .runtime import configure_default_workflow
 from .workflow import build_workflow
 
 __all__ = [
@@ -8,5 +9,6 @@ __all__ = [
     "OrchestratorResponse",
     "b3_orchestrator",
     "build_workflow",
+    "configure_default_workflow",
     "configure_workflow",
 ]
