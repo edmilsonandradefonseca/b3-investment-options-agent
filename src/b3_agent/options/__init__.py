ca616cc6a714d6dedadd302dcb6c1f1194f3ce78
EDMILSON DE ANDRADE FONSECA <edmilson.afonseca@gmail.com>
2026-09-19 15:03:17 -0300

    feat(options): add brokerage note ledger foundation

--- src/b3_agent/options/__init__.py
+++ src/b3_agent/options/__init__.py
@@ -1,9 +1,12 @@
 from .analysis import OptionsAnalysis, OptionsAnalysisEngine
+from .brokerage_notes import BrokerageNoteIngestionError, BrokerageNoteParser
 from .call import CallAnalysisEngine, CallOpportunity
 from .policy import OptionsPolicy
 from .put import PutAnalysisEngine, PutOpportunity
 
 __all__ = [
+    "BrokerageNoteIngestionError",
+    "BrokerageNoteParser",
     "CallAnalysisEngine",
     "CallOpportunity",
     "OptionsAnalysis",
