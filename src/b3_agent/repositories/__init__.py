c6b0bdb0598f4bc33ebe57c8372d75e7bcbf89a2
EDMILSON DE ANDRADE FONSECA <edmilson.afonseca@gmail.com>
2026-09-19 15:36:26 -0300

    feat(options): export contract registry

--- src/b3_agent/repositories/__init__.py
+++ src/b3_agent/repositories/__init__.py
@@ -1,4 +1,10 @@
+from .option_contract_registry import OptionContractRecord, OptionContractRegistry
 from .option_ledger import OptionTransactionLedger
 from .portfolio import PortfolioRepository
 
-__all__ = ["OptionTransactionLedger", "PortfolioRepository"]
+__all__ = [
+    "OptionContractRecord",
+    "OptionContractRegistry",
+    "OptionTransactionLedger",
+    "PortfolioRepository",
+]
