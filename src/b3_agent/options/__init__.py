2a0158f2e040a439629dfab6e9c8e5b57c89a731
EDMILSON DE ANDRADE FONSECA <edmilson.afonseca@gmail.com>
2026-09-19 15:30:39 -0300

    feat(options): export lifecycle engine

--- src/b3_agent/options/__init__.py
+++ src/b3_agent/options/__init__.py
@@ -3,6 +3,7 @@ from .brokerage_notes import BrokerageNoteIngestionError, BrokerageNoteParser
 from .call import CallAnalysisEngine, CallOpportunity
 from .policy import OptionsPolicy
 from .put import PutAnalysisEngine, PutOpportunity
+from .lifecycle import OptionContract, OptionLifecycle, OptionLifecycleEngine, build_option_lifecycles
 
 __all__ = [
     "BrokerageNoteIngestionError",
@@ -14,4 +15,8 @@ __all__ = [
     "OptionsPolicy",
     "PutAnalysisEngine",
     "PutOpportunity",
+    "OptionContract",
+    "OptionLifecycle",
+    "OptionLifecycleEngine",
+    "build_option_lifecycles",
 ]
