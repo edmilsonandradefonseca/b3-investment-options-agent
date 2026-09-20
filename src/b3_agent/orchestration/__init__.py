b7bbebb68d8b78d9cb696e6dc27343ecda946ac9
EDMILSON DE ANDRADE FONSECA <edmilson.afonseca@gmail.com>
2026-09-19 21:51:05 -0300

    feat: export deterministic dashboard workflow

--- src/b3_agent/orchestration/__init__.py
+++ src/b3_agent/orchestration/__init__.py
@@ -1,6 +1,6 @@
 from .contracts import B3State, OrchestratorRequest, OrchestratorResponse
 from .orchestrator import b3_orchestrator, configure_workflow
-from .runtime import configure_default_workflow
+from .runtime import configure_dashboard_workflow, configure_default_workflow
 from .workflow import build_workflow
 
 __all__ = [
@@ -10,5 +10,6 @@ __all__ = [
     "b3_orchestrator",
     "build_workflow",
     "configure_default_workflow",
+    "configure_dashboard_workflow",
     "configure_workflow",
 ]
